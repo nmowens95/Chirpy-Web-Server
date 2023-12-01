b922172512d83f76c013489e14de65b00e5a6980
nmowens95 <nmowens95@gmail.com>
2023-12-01 15:46:45 -0600

    provided server side logic for invalid id infromation

--- handler_chirps_get.go
+++ handler_chirps_get.go
@@ -3,7 +3,30 @@ package main
 import (
 	"net/http"
 	"sort"
+	"strconv"
+
+	"github.com/go-chi/chi/v5"
 )
+
+func (cfg *apiConfig) handlerChirpsGet(w http.ResponseWriter, r *http.Request) {
+	chirpIDString := chi.URLParam(r, "chirpID") // taking a specific ID per chirp
+	chirpID, err := strconv.Atoi(chirpIDString) // convert ID to int (ASCII to integer)
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "Invalid Chirp ID")
+		return
+	}
+
+	dbChirp, err := cfg.DB.GetChirp(chirpID)
+	if err != nil {
+		respondWithError(w, http.StatusNotFound, "Couldn't get Chirp")
+		return
+	}
+
+	respondWithJSON(w, http.StatusOK, Chirp{
+		ID:   dbChirp.ID,
+		Body: dbChirp.Body,
+	})
+}
 
 func (cfg *apiConfig) handlerChirpsRetrieve(w http.ResponseWriter, r *http.Request) {
 	dbChirps, err := cfg.DB.GetChirps()
