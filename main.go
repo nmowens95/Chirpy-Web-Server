a438d770080f5f4ca7057a21163ad6983561541c
nmowens95 <nmowens95@gmail.com>
2023-12-30 16:11:35 -0600

    Added a jwt token and loaded it from our .env file. Also added a debug flag to auto delete our database. Now have a handler for handlerUsersUpdate too

--- main.go
+++ main.go
@@ -1,16 +1,20 @@
 package main
 
 import (
+	"flag"
 	"log"
 	"net/http"
+	"os"
 
 	"github.com/go-chi/chi/v5"
+	"github.com/joho/godotenv"
 	"github.com/nmowens95/Chirpy-Web-Server/internal/database"
 )
 
 type apiConfig struct {
 	fileserverHits int
 	DB             *database.DB
+	jwtSecret      string
 }
 
 // using our own files for a DB currently
@@ -19,14 +23,31 @@ func main() {
 	const filePathRoot = "."
 	const port = "8080"
 
+	godotenv.Load(".env")
+
+	jwtSecret := os.Getenv("JWT_SECRET")
+	if jwtSecret == "" {
+		log.Fatal("JWT_SECRET environment variable is not set")
+	}
+
 	db, err := database.NewDB("database.json")
 	if err != nil {
 		log.Fatal(err)
 	}
 
+	dbg := flag.Bool("debug", false, "Enable debug mode")
+	flag.Parse()
+	if dbg != nil && *dbg {
+		err := db.ResetDB()
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
+
 	apiCfg := apiConfig{
 		fileserverHits: 0,
 		DB:             db,
+		jwtSecret:      jwtSecret,
 	}
 
 	router := chi.NewRouter()
@@ -40,18 +61,18 @@ func main() {
 	apiRouter.Get("/reset", apiCfg.handlerReset)
 
 	apiRouter.Post("/login", apiCfg.handlerLogin)
+
 	apiRouter.Post("/users", apiCfg.handlerUsersCreate)
+	apiRouter.Put("/users", apiCfg.handlerUsersUpdate)
 
 	apiRouter.Post("/chirps", apiCfg.handlerChirpsCreate)
 	apiRouter.Get("/chirps", apiCfg.handlerChirpsRetrieve)
 	apiRouter.Get("/chirps/{chirpID}", apiCfg.handlerChirpsGet)
 	router.Mount("/api", apiRouter)
-	// Backend focus
 
 	adminRouter := chi.NewRouter()
 	adminRouter.Get("/metrics", apiCfg.handlerMetrics)
 	router.Mount("/admin", adminRouter)
-	// Backend focus
 
 	corsMux := middlewareCors(router) // Cross-Origin-Resource-Sharing
 
